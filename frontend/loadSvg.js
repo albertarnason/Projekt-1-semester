785ee32bdb123da560380f2e3c4a106fba6b71ae
albertarnason <albertingerslevarnason@gmail.com>
2025-05-19 09:44:13 +0200

    website fix 3

--- frontend/loadSvg.js
+++
@@ -1,5 +0,0 @@
-fetch("svg.html") // henter filen svg.html
-  .then((response) => response.text()) // Når det er gjort, konverterer det til tekst (HTML-strenge)
-  .then((html) => {
-    document.getElementById("svg-pladsholder").innerHTML = html;
-  });
