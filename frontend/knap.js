85c1e3c35f811744ee1e381f04fbeb03d6d983d1
1sra0mar <israaaomar22@gmail.com>
2025-05-22 09:48:46 +0200

    Farvebox tilføjet

--- frontend/knap.js
+++ frontend/knap.js
@@ -1,16 +1,13 @@
-// Hent elementet med id 'toggle' (checkboxen)
+
 const toggle = document.getElementById('toggle');
 
-// Hent elementet med id 'thumbText' (den bevægelige tekst i slideren)
 const thumbText = document.getElementById('thumbText');
 
-// Lyt efter ændringer på 'toggle'-input (når checkboxen bliver ændret)
 toggle.addEventListener('change', function () {
-  // Hvis checkboxen er checked (slideren er til højre eller venstre)
   if (this.checked) {
-    thumbText.textContent = "Salg"; // Sætter teksten i slideren til "Salg"
+    thumbText.textContent = "Salg"; 
   } else {
-    thumbText.textContent = "Produktion"; // Sætter teksten i slideren til "Produktion"
+    thumbText.textContent = "Produktion"; 
   }
 });
 
